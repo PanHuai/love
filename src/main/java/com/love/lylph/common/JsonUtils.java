c66101cc313dbcfcf3389e656bc91910c846a615
panhuai <248187023@qq.com>
2018-08-02 19:10:17 +0800

    0.0.1 commit

--- src/main/java/com/love/lylph/common/JsonUtils.java
+++ src/main/java/com/love/lylph/common/JsonUtils.java
@@ -1,10 +1,18 @@
 package com.love.lylph.common;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.love.lylph.pojo.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @author PanHuai
@@ -14,20 +22,80 @@ public class JsonUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(JsonUtils.class);
 
-    private static final Gson GSON = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+    private static final Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
 
-    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    private static final JsonUtils json = new JsonUtils();
+
+    public static JsonUtils getJsonUtils() {
+        return json ;
+    }
 
     /**
      * 将对象转化为json字符串
      */
-    public static String tojson() {
+    public String gsonToJson(Object o) {
+        if (o == null) {
+            return null;
+        }
+        return gson.toJson(o);
+    }
 
+    public String jacksonToJson(Object o) {
+        if (o == null) {
+            return null;
+        }
+        try {
+            return mapper.writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            logger.error(String.format("o=[%s]", o.toString()), e);
+        }
         return null;
     }
 
+    /**
+     * 将json转化为对象
+     */
+    public <T> T jacksonfromJson(String json,Class<T> clazz){
+        if(json == null){
+            return null;
+        }
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (IOException e) {
+            logger.error(String.format("json=[%s]", json), e);
+        }
+        return null;
+    }
+
+    public <T> T gsonfromJson(String json,Class<T> clazz){
+        if (json == null) {
+            return  null;
+        }
+        return gson.fromJson(json, clazz);
+    }
+
+    /**
+     * 将json转化为集合
+     */
+    public <T> Collection<T> fromJson(String json,Class<? extends Collection> collectionClazz,Class<T> clazz) {
+        return gson.fromJson(json,new TypeToken<Collection<T>>(){}.getType());
+    }
 
 
+    public static void main(String[] args) {
 
+        List<User> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            User user = new User();
+            user.setName("pan");
+            list.add(user);
+        }
+
+        System.out.println(json.gsonToJson(list));
+
+
+    }
 
 }
